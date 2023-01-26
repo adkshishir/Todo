79e1653ab0a9d23ecc49edac7e0946ad685703e4
adkshishir <98789290+adkshishir@users.noreply.github.com>
2023-01-26 07:59:47 +0545

    updates

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,6 +4,9 @@ import NightlightIcon from "@mui/icons-material/Nightlight";
 import Form from "./Form";
 import img from "../images/bg-desktop-dark.jpg";
 import dayImg from "../images/bg-desktop-light.jpg";
+import nightMobImg from "../images/bg-mobile-dark.jpg";
+import dayMobImg from "../images/bg-mobile-light.jpg";
+import Media from "react-media";
 const MainContent = () => {
   const [isday, setday] = useState(false);
   const [styles, setStyles] = useState({
@@ -36,11 +39,29 @@ const MainContent = () => {
         height: "100vh",
       }}
     >
-      <img
-        src={isday ? dayImg : img}
-        alt="background"
-        style={{ width: "100vw", height: "30vh" }}
-      />
+      <Media
+        queries={{
+          desktop: "(min-width: 600px)",
+          mobile: "(max-width: 500px)",
+        }}
+      >
+        {(matches) =>
+          matches.desktop ? (
+            <img
+              src={isday ? dayImg : img}
+              alt="background"
+              style={{ width: "100vw", height: "30vh" }}
+            />
+          ) : (
+            <img
+              src={isday ? dayMobImg : nightMobImg}
+              alt="background"
+              style={{ width: "100vw", height: "30vh" }}
+            />
+          )
+        }
+      </Media>
+
       <div className="container">
         <div style={{ color: "white" }} className=" item1">
           <div className="todo-head">TODO</div>
